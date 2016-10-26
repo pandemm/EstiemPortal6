35b49d84ef18113f7538b0701f6a317e3ade7756
pandemm <lassi.uosukainen@gmail.com>
2016-10-26 13:58:57 +0300

    Fixed relative links within news items

--- EstiemPortal6/Controllers/NewsController.cs
+++ EstiemPortal6/Controllers/NewsController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using EstiemPortal6.Repositories;
 using EstiemPortal6.Models;
+using System.Text.RegularExpressions;
 
 namespace EstiemPortal6.Controllers
 {
@@ -14,7 +15,18 @@ namespace EstiemPortal6.Controllers
         {
             var repo = new NewsRepository();
             News news = repo.GetNewsById(id);
+            // News items have relative links.
+            // Replace relative links with absolute links
+            Regex rx = new Regex(@"src=""/[iI]nternal/");
+            news.Text = rx.Replace(news.Text, new MatchEvaluator(ReplaceImageUrl));
             return View(news);
+        }
+
+        // Move this function to some better class. Helpers?
+        static string ReplaceImageUrl(Match m)
+        {
+            string x = m.ToString();
+            return ("src=\"https://www.estiem.org/internal/");
         }
 
         public ActionResult NewsList(int id)
