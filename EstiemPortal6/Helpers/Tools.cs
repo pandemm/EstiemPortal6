85fb009f5bb1659fe5d1a196bdfaca7922730b2e
pandemm <lassi.uosukainen@gmail.com>
2016-10-29 22:46:33 +0300

    Added Favicon and Wiki link

---
+++ EstiemPortal6/Helpers/Tools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace EstiemPortal6.Helpers
+{
+    public class Helpers
+    {
+
+    }
+}
